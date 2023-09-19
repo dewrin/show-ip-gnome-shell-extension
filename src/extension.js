507a70aa0e07836f0cbd810a2f1e848b1cf4528f
Rincahya Maragung <www.dewrin@gmail.com>
2023-09-19 13:05:07 +0700

    Update extension.js

--- src/extension.js
+++ src/extension.js
@@ -28,7 +28,7 @@ class Extension {
     	button.connect('clicked', () => {  this.update_info(button) });
         this._indicator.add_child(button);
         Main.panel.addToStatusArea(indicatorName, this._indicator);
-        this._sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, () => {
+        this._sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
             this.update_info(button)
             return true; 
         });
